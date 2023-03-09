1bd813cfdfe281ca765e296dcf05fd24a134edde
krisbitney <kristoferbitney@hotmail.com>
2023-03-09 14:15:10 +0530

    set up test layout

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -1,8 +1,48 @@
+import {PolywrapClient} from "@polywrap/client-js";
+import * as Types from "./types/wrap";
+import {Commands} from "@polywrap/cli-js";
+import * as path from "path";
 
 
 jest.setTimeout(360000);
 
 describe("Gelato Relay Wrapper", () => {
+  const client = new PolywrapClient()
+  const uri: string = "wrap://fs/../../build";
 
+  beforeAll(async () => {
+    await Commands.build(undefined, { cwd: path.join(__dirname, "../..") });
+  });
+
+  test("callWithSyncFee should return a valid response", async () => {
+    // const request = {
+    //     chainId: BigInt!
+    //     target: String!
+    //     data: Bytes!
+    //     feeToken: String!
+    //     isRelayContext: Boolean
+    //   };
+    // const options = {
+    //   gasLimit: BigInt
+    //   retries: Int
+    // };
+    // const result = client.invoke<Types.RelayResponse>({
+    //   uri,
+    //   method: "callWithSyncFee",
+    //   args: { request, options }
+    // });
+  });
+
+  test("sponsoredCall should return a valid response", async () => {
+
+  });
+
+  test("getEstimatedFee should return a valid response", async () => {
+
+  });
+
+  test("getTaskStatus should return a valid response", async () => {
+
+  });
 })
 
